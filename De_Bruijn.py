32e07865f45526d8de7ecfffc1d019e23daae0ef
zeroes <zeroesskeys@gmail.com>
2019-03-18 21:26:38 +1100

    Create De_Bruijn.py

---
+++ De_Bruijn.py
@@ -0,0 +1,36 @@
+# Code from Wikipedia page:
+# https://en.wikipedia.org/wiki/De_Bruijn_sequence
+# from https://cdn.hackaday.io/files/274451174206880/DeBruijn.py
+
+def de_bruijn(k, n):
+    """
+    de Bruijn sequence for alphabet k
+    and subsequences of length n.
+    """
+    try:
+        # let's see if k can be cast to an integer;
+        # if so, make our alphabet a list
+        _ = int(k)
+        alphabet = list(map(str, range(k)))
+
+    except (ValueError, TypeError):
+        alphabet = k
+        k = len(k)
+
+    a = [0] * k * n
+    sequence = []
+
+    def db(t, p):
+        if t > n:
+            if n % p == 0:
+                sequence.extend(a[1:p + 1])
+        else:
+            a[t] = a[t - p]
+            db(t + 1, p)
+            for j in range(a[t - p] + 1, k):
+                a[t] = j
+                db(t + 1, t)
+    db(1, 1)
+    return "".join(alphabet[i] for i in sequence)
+
+print(de_bruijn("0123", 4))
